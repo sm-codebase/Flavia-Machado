59ba72621420198f7f336999125a3f7bd7e806c7
Vinicius Nascimento <viniciusnicacio80@gmail.com>
2024-01-05 18:06:14 -0300

    atualizando

--- components/Services.jsx
+++ components/Services.jsx
@@ -4,17 +4,15 @@ import Image from "next/image";
 const services = () => {
   return (
     <div id="services" className="container mx-auto text-black p-5 ">
-      <div className="grid justify-items-center  ">
-        <div>
-        <div className="grid text-center text-white p-6 ">
-        <h2>Faça seu evento conosco</h2>
+      <div className="grid justify-items-center">
+        <div className="grid text-center text-white p-6 text-2xl">
+          <p>Faça seu evento conosco</p>
         </div>
-          <video
-            controls
-            src={"/assets/videos/video-1.mp4"}
-            style={{ width: "720px", height: "480px" }}
-          />
-        </div>
+        <video
+          controls
+          src={"/assets/videos/video-1.mp4"}
+          style={{ width: "720px", height: "480px" }}
+        />
       </div>
     </div>
   );
